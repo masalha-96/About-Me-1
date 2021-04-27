bb40c54942b882055c8aabb3baf6c5fd40e85117
Bashar-Syouri <beshoo_1993@hotmail.com>
2021-04-27 19:19:23 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 'use strict'
 
+let score = 0;
 let userName = prompt('Hello, Could you please enter your name');
 console.log('Your name is : '+userName);
 alert('Hello '+userName +' Welcome to my website, best wishes :) ');
 
-let myName = prompt('Do you know my name? Please answer yes or no y/n !');
+let myName = prompt('Do you know what my name is?? Please answer yes or no y/n !');
 console.log('Your answer is: '+myName);
 alert('Your answer is: '+myName);
 
@@ -12,13 +13,14 @@ alert('Your answer is: '+myName);
 switch (myName.toUpperCase()) {
     case 'YES':
     case 'Y':
-        console.log('Great you are know my name, have a nice day');
-        alert('Great, you are know my name, have a nice day');
+        console.log('Great you know what my name is, have a nice day');
+        alert('Great, you know what my name is, have a nice day');
+        score++;
         break;
     case 'NO':
     case 'N':   
-         console.log('Ooh no , you do not know my name, and my name is Bashar , Nice to meet you :)');
-         alert('Ooh no , you do not know my name, and my name is Bashar , Nice to meet you :)');
+         console.log('Ooh no , you do not know what my name is, and my name is Bashar , Nice to meet you :)');
+         alert('Ooh no , you do not know what my name is, and my name is Bashar , Nice to meet you :)');
          break;
     default:
         alert('Please Take care to your self :)');         
@@ -35,6 +37,7 @@ switch (gym.toUpperCase()) {
     case 'Y':
         console.log('Great, I like go the the gym, I hope to meet you there :)');
         alert('Great, I like go the the gym, I hope to meet you there :)');
+        score++;
         break;
     case 'No':
     case 'N':
@@ -57,6 +60,7 @@ switch (movies.toUpperCase()) {
     case 'Y':
         console.log('Great, I am like movies, So you are welcome to my website now :)');
         alert('Great, I am like movies, So you are welcome to my website now :)');
+        score++;
         break;
     case 'No':
     case 'N':
@@ -79,6 +83,7 @@ switch (swim.toUpperCase()) {
     case 'Y':
         console.log('Great, I am like a swimming, I hope you to like a swim also :)');
         alert('Great, I am like a swimming, I hope you to like a swim also :)');
+        score++;
         break;
     case 'No':
     case 'N':
@@ -102,6 +107,7 @@ switch (study.toUpperCase()) {
     case 'Y':
         console.log('Good, You know what I studied :)');
         alert('Good, You know what I studied :)');
+        score++;
         break;
     case 'No':
     case 'N':
@@ -111,3 +117,50 @@ switch (study.toUpperCase()) {
         default:
             alert('Please Take care to your self :)');
 }
+
+let count =0;
+  let myOld = Number(prompt('How old I am ?'));
+  while (!myOld) {
+         myOld = Number(prompt('How old I am ?'));
+         }
+     for (let i = 0; i < 3; i++) 
+     {
+        if (myOld !== 28) {
+            alert('too bad my old not '+myOld);
+            if(myOld>26 && myOld<30)
+                {alert('your guess is too high');}
+                 else
+                  {alert('Your guess is too low');}
+        count=count+1;
+        myOld = Number(prompt('How old I am ?'));}
+           else
+           {alert('hello my old is correct is ' + myOld );
+           score++;
+           break; }
+       if(count==3) 
+       {alert('thanks for trying, the correct answer is 28');
+       break;}
+    }
+
+    let car = ['Koenigsegg','Lamborghini','Mansory Vivere Bugatti Veyron','Ferrari LaFerrari'];
+    let userAnswer;
+    for(let i = 1 ; i <=6 ; i++){
+        userAnswer = prompt('Name One of car from my Top 10 Most Expensive Cars In The World');
+
+        if(userAnswer .toLocaleLowerCase() === car[0] || userAnswer === car[1] || userAnswer === car[2] || userAnswer === car[3]){
+            alert('You are correct');
+            score++
+            break;
+        }
+        else {
+            alert('Wrong answer Please try again')
+        }
+    }
+    alert(car[0]+'/ '+ car[1] +'/ '+ car[2] +'/ ' + car[3]);
+
+    alert('Your score is ' + score + ' out of 7');
+
+    alert('You Did A Great Job , Thank You ' + userName +' :)');
+
+    
+    
